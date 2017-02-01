1365686574ba6364cadb4a5a955660bc7b118513
Makoto <mlsaito@live.com>
2017-02-01 15:30:17 +0900

    Created a reusable button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   // instantiates variables from album props instead of props.album.title
@@ -28,11 +29,16 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image
           style={imageStyle}
           source={{ uri: image }}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => console.log(title)} />
       </CardSection>
     </Card>
   );
